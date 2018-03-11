aa1f4d20c0c0787685bd978f1fed2910fca1e7ee
Kevin Malkic <k_malkic@yahoo.fr>
2018-03-11 19:13:07 +0000

    project cleanup

---
+++ TankDemo/TankDemo/Classes/GameViewController.swift
@@ -0,0 +1,19 @@
+
+import UnityKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
