da45e170585ca2cda182661ec2e9ec302944c0fb
Kevin Malkic <k_malkic@yahoo.fr>
2018-03-13 22:43:13 +0000

    colliders in file

---
+++ UnityKit/UnityKit/Components/BoxCollider.swift
@@ -0,0 +1,76 @@
+
+import SceneKit
+
+public class BoxCollider: Collider {
+
+    override func constructBody() {
+
+        guard let gameObject = gameObject
+            else { return }
+
+        let boundingBox = gameObject.node.boundingBox * gameObject.transform.localScale
+        let vertices = [Vector3(boundingBox.min.x, boundingBox.min.y, boundingBox.min.z),
+                        Vector3(boundingBox.min.x, boundingBox.max.y, boundingBox.min.z),
+                        Vector3(boundingBox.max.x, boundingBox.max.y, boundingBox.min.z),
+                        Vector3(boundingBox.max.x, boundingBox.min.y, boundingBox.min.z),
+                        Vector3(boundingBox.max.x, boundingBox.min.y, boundingBox.max.z),
+                        Vector3(boundingBox.max.x, boundingBox.max.y, boundingBox.max.z),
+                        Vector3(boundingBox.min.x, boundingBox.max.y, boundingBox.max.z),
+                        Vector3(boundingBox.min.x, boundingBox.min.y, boundingBox.max.z)]
+
+        let indices: [Int16] = [0, 1, 2,
+                                0, 2, 3,
+                                3, 2, 5,
+                                3, 5, 4,
+                                5, 2, 1,
+                                5, 1, 6,
+                                3, 4, 7,
+                                3, 7, 0,
+                                0, 7, 6,
+                                0, 6, 1,
+                                4, 5, 6,
+                                4, 6, 7]
+
+        let normals = [Vector3(0, 0, 1),
+                       Vector3(0, 0, 1),
+                       Vector3(0, 0, 1),
+                       Vector3(0, 0, 1),
+                       Vector3(0, 0, 1),
+                       Vector3(0, 0, 1),
+                       Vector3(0, 0, 1),
+                       Vector3(0, 0, 1)]
+
+        let vertexData = Data(bytes: vertices, count: vertices.count * MemoryLayout<Vector3>.size)
+        let vertexSource = SCNGeometrySource(data: vertexData,
+                                             semantic: .vertex,
+                                             vectorCount: vertices.count,
+                                             usesFloatComponents: true,
+                                             componentsPerVector: 3,
+                                             bytesPerComponent: MemoryLayout<Float>.size,
+                                             dataOffset: 0,
+                                             dataStride: MemoryLayout<Vector3>.size)
+
+        let normalData = Data(bytes: normals, count: normals.count * MemoryLayout<Vector3>.size)
+        let normalSource = SCNGeometrySource(data: normalData,
+                                             semantic: .normal,
+                                             vectorCount: normals.count,
+                                             usesFloatComponents: true,
+                                             componentsPerVector: 3,
+                                             bytesPerComponent: MemoryLayout<Float>.size,
+                                             dataOffset: 0,
+                                             dataStride: MemoryLayout<Vector3>.size)
+
+
+
+        let indexData = Data(bytes: indices, count: indices.count * MemoryLayout<Int16>.size)
+        let element = SCNGeometryElement(data: indexData,
+                                         primitiveType: .triangles,
+                                         primitiveCount: indices.count / 3,
+                                         bytesPerIndex: MemoryLayout<Int16>.size)
+
+        let geometry = SCNGeometry(sources: [vertexSource, normalSource], elements: [element])
+
+        updatePhysicsShape(SCNPhysicsShape(geometry: geometry))
+        createVisibleCollider(geometry)
+    }
+}
---
+++ UnityKit/UnityKit/Components/PlaneCollider.swift
@@ -0,0 +1,56 @@
+
+import SceneKit
+
+public class PlaneCollider: Collider {
+
+    override func constructBody() {
+
+        guard let gameObject = gameObject
+            else { return }
+
+        let boundingBox = gameObject.node.boundingBox * gameObject.transform.localScale
+        let vertices = [Vector3(boundingBox.min.x, boundingBox.max.y, boundingBox.min.z), //1 //0
+            Vector3(boundingBox.max.x, boundingBox.max.y, boundingBox.min.z), //2 //1
+            Vector3(boundingBox.max.x, boundingBox.max.y, boundingBox.max.z), //5 //2
+            Vector3(boundingBox.min.x, boundingBox.max.y, boundingBox.max.z)] //6 //3
+
+        let indices: [Int16] = [2, 1, 0,
+                                3, 2, 0]
+
+        let normals = [Vector3(0, 1, 1),
+                       Vector3(0, 1, 1),
+                       Vector3(0, 1, 1),
+                       Vector3(0, 1, 1)]
+
+        let vertexData = Data(bytes: vertices, count: vertices.count * MemoryLayout<Vector3>.size)
+        let vertexSource = SCNGeometrySource(data: vertexData,
+                                             semantic: .vertex,
+                                             vectorCount: vertices.count,
+                                             usesFloatComponents: true,
+                                             componentsPerVector: 3,
+                                             bytesPerComponent: MemoryLayout<Float>.size,
+                                             dataOffset: 0,
+                                             dataStride: MemoryLayout<Vector3>.size)
+
+        let normalData = Data(bytes: normals, count: normals.count * MemoryLayout<Vector3>.size)
+        let normalSource = SCNGeometrySource(data: normalData,
+                                             semantic: .normal,
+                                             vectorCount: normals.count,
+                                             usesFloatComponents: true,
+                                             componentsPerVector: 3,
+                                             bytesPerComponent: MemoryLayout<Float>.size,
+                                             dataOffset: 0,
+                                             dataStride: MemoryLayout<Vector3>.size)
+
+        let indexData = Data(bytes: indices, count: indices.count * MemoryLayout<Int16>.size)
+        let element = SCNGeometryElement(data: indexData,
+                                         primitiveType: .triangles,
+                                         primitiveCount: indices.count / 3,
+                                         bytesPerIndex: MemoryLayout<Int16>.size)
+
+        let geometry = SCNGeometry(sources: [vertexSource, normalSource], elements: [element])
+
+        updatePhysicsShape(SCNPhysicsShape(geometry: geometry))
+        createVisibleCollider(geometry)
+    }
+}
