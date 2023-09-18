36fe46c3d47de737abcb87f4fa01c4d9f35cf88c
Kevin Malkic <kevin@saltandpeppercode.com>
2023-09-18 11:18:41 +0100

    bump

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "UnityKit",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "UnityKit", targets: ["UnityKit"]),
     ],
