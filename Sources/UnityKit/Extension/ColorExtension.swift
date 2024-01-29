89df21948b87aa75421faa0a265ae8d2fdd4f137
Kevin Malkic <k_malkic@yahoo.fr>
2024-01-29 13:20:46 +0000

    fix

--- Sources/UnityKit/Extension/ColorExtension.swift
+++ Sources/UnityKit/Extension/ColorExtension.swift
@@ -19,12 +19,7 @@ public extension Color {
         }
         let start = hex.index(hex.startIndex, offsetBy: 1)
         let hexColor = String(hex[start...])
-        
-        guard hexColor.count == 8 else {
-            self.init(red: 1, green: 0, blue: 0, alpha: 1)
-            return
-        }
-        
+                
         let scanner = Scanner(string: hexColor)
         var hexNumber: UInt64 = 0
         
@@ -36,7 +31,7 @@ public extension Color {
         r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
         g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
         b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-        a = CGFloat(hexNumber & 0x000000ff) / 255
+        a = hexColor.count == 8 ? CGFloat(hexNumber & 0x000000ff) / 255 : 1
         
         self.init(red: r, green: g, blue: b, alpha: a)
         return
